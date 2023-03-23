16cc152036d7d04215c4c3aa3f57b0f2c90e2165
jiu7004 <tara7004@gmail.com>
2023-03-23 13:14:30 +0000

    the first comit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args) 
+
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+// class : c -  절차지향 언어 / c++부터 객체지향 
+//  private static void Main(string[] args) -> 진입점
+// WriteLine("") -> 함수 - printf와 비슷한 애  -> 자료형을 정하지 않아도 괜찮다
+//Console : 출력할 대상, 입출력을 하는 매개 
+// 지금은 컴파일러는 있는데 실행할 방법이 없다. 
+// NET -> 구조적으로 따지면  닷넷라이브러리에서 제공하는 출력하는 기능을 구현해놓은 기능의 함수 
+// json  : 환경 파일 - 환경: 컴파일 할 때 어떻게 할건지... 환경 이름을 다른 이름들도 바뀐다 
+// 
